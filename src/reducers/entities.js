6d06aa95ce89a88ce65706420b07f64f22f17abf
YasirYang <yingliang.yang@xiaoniangao.cn>
2018-08-06 12:26:09 +0800

    数据扁平化

--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -4,52 +4,53 @@ import ActionTypes from '../const/ActionTypes'
 
 function classes (state = {},action){
   switch(action.type){
-    case `${ActionTypes.FETCH_LESSON_SATISFIED_INFO}_SUC`:
-    {
+    case `${ActionTypes.FETCH_LESSON_SATISFIED_INFO}_SUC`: {
       const entities =  action.response.entities
       return {
         ...state,
         ...entities.classes
       };
     }
+    default:
+      return state;
   }
-  return state;
 }
 
 function teachers (state = {},action){
   switch(action.type){
-    case `${ActionTypes.FETCH_LESSON_SATISFIED_INFO}_SUC`:
-    {
+    case `${ActionTypes.FETCH_LESSON_SATISFIED_INFO}_SUC`: {
       const entities =  action.response.entities
       return {
         ...state,
         ...entities.teachers
       };
     }
+    default:
+      return state;
   }
-  return state;
 }
 
 function satisfiled (state = {},action){
   switch(action.type){
-    case `${ActionTypes.FETCH_LESSON_SATISFIED_INFO}_SUC`:
-    {
+    case `${ActionTypes.FETCH_LESSON_SATISFIED_INFO}_SUC`: {
       const entities =  action.response.entities
       return {
         ...state,
         ...entities.satisfiled
       };
     }
-    case `${ActionTypes.REPLY_USER_FEED_BACK}`:
-    const { time } = action.params
-    
-    const targetItem = { ...state[time] }
-    targetItem.reply_status = 1
+    case `${ActionTypes.REPLY_USER_FEED_BACK}`: {
+      const { time } = action.params
+      
+      const targetItem = { ...state[time] }
+      targetItem.reply_status = 1
 
-    const nextState = { ...state, [time]: targetItem }
-    return nextState
+      const nextState = { ...state, [time]: targetItem }
+      return nextState
+    }
+    default:
+      return state;
   }
-  return state;
 }
 
 export default combineReducers({
